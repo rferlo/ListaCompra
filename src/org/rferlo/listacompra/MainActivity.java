e69816356bfa7b936e576615a5500fabe97ec658
Rafael Fernández <rferlo@gmail.com>
2013-05-31 07:45:45 +0200

    Integrada parte de datos

--- src/org/rferlo/listacompra/MainActivity.java
+++ src/org/rferlo/listacompra/MainActivity.java
@@ -1,8 +1,15 @@
 package org.rferlo.listacompra;
 
-import android.os.Bundle;
+import java.util.Vector;
+
+import org.rferlo.listacompra.controlador.Base;
+import org.rferlo.listacompra.modelo.Unidad;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +17,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		/* Esto es un comentario para el GIT */
+		
+		TextView tx = (TextView)findViewById(R.id.tx01);
+
+		// Arrancar el entorno
+		Base.InicializaEntorno();
+		Log.d("UNIDADES" ,Base.getListaUnidades());
+		
+		Vector<Unidad> v = Base.getListaUnidadesEmpiezaPor("g");
+		System.out.println("UNIDADES QUE CONTIENEN G");
+		StringBuffer sb = new StringBuffer();
+		for(Unidad u: v) {
+			sb.append(u.toString() + "\n");
+		}
+		tx.setText(sb.toString());
+
+		
 	}
 
 	@Override
---
+++ src/org/rferlo/listacompra/pruebas.java
@@ -0,0 +1,27 @@
+package org.rferlo.listacompra;
+
+import java.util.Vector;
+
+import org.rferlo.listacompra.controlador.Base;
+import org.rferlo.listacompra.modelo.Unidad;
+
+public class pruebas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Arrancar el entorno
+		Base.InicializaEntorno();
+		System.out.println(Base.getListaUnidades());
+		
+		Vector<Unidad> v = Base.getListaUnidadesEmpiezaPor("g");
+		System.out.println("UNIDADES QUE CONTIENEN G");
+		for(Unidad u: v) {
+			System.out.println(u.toString());
+		}
+		
+
+	}
+
+}
